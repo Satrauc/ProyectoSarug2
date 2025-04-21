15009eedba150d6cabfef32f531b4d54eb47c83c
Satrauc <fabian.cuartas@correounivalle.edu.co>
2025-04-21 05:26:24 -0500

    Actualizacion en Detección de planta

--- Arduino/SARU-G2/DeteccionPlanta.ino
+++ Arduino/SARU-G2/DeteccionPlanta.ino
@@ -1,3 +1,17 @@
-void funcion_modularD() {
-    Serial.println("Hola desde Detección de la planta .ino");
+// COnfiguración del pin
+void ConfigQRE1113(int QRE_PIN){
+  pinMode(QRE_PIN, INPUT);
+
+}
+
+// Función para detectar un objeto con el sensor QRE1113
+bool detectarObjeto(int UMBRAL, int QRE_PIN) {
+  int lectura = analogRead(QRE_PIN);  // Leemos el valor del sensor (0-1023)
+  
+  // Si la lectura es menor que el umbral, significa que el sensor detectó un objeto
+  if (lectura < UMBRAL) {
+    return true;  // Objeto detectado
+  } else {
+    return false; // No se detectó objeto
+  }
 }
