95dcfeff76d8be31fd1ce335bebb9cc7fdb90602
Satrauc <121595738+Satrauc@users.noreply.github.com>
2025-05-01 13:10:00 -0500

    Update SARU-G2.ino
    Se agrega Ejemplo para uso de QRE1114

--- Arduino/SARU-G2/SARU-G2.ino
+++ Arduino/SARU-G2/SARU-G2.ino
@@ -6,3 +6,22 @@ void loop() {
     //funcion_modular();  // Llamada a la función que está en otro archivo
     delay(1000);
 }
+/******************************************************************************/
+//Ejemplo QRE1114
+/******************************************************************************/
+/**
+const int sensores[] = {A0, A1, A2};  // Usa de 1 a 4 sensores
+const int cantidadSensores = 3;
+const int umbral = 500;
+
+void setup() {
+  Serial.begin(9600);
+  configurarSensores(sensores, cantidadSensores);
+}
+
+void loop() {
+  bool objeto = detectarObjetoRedundante(umbral, sensores, cantidadSensores);
+  Serial.println(objeto ? "Objeto detectado" : "Sin objeto");
+  delay(200);
+}
+/**/
