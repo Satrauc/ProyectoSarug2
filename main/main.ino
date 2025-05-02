8f2fccc3e837bf15d251adcbb022fcf858d371ef
Carlos Bustos Olaya <87876295+eleventhcurve91@users.noreply.github.com>
2025-05-01 23:38:45 -0500

    Comentarios para facilitar el entendimiento de algunos

--- main/main.ino
+++ main/main.ino
@@ -2,14 +2,37 @@ int MotorPins[] = {1, 2, 3, 4};
 int Pwm[] = {2, 3};
 int EncoderPins[] = {5, 6};
 
-
 void setup() {
-  // Manual de uso
-  ConfigMotor(MotorPins, Pwm, 6); //(Motores, Pwm, BaseSpeed, Stby)
-  Encoder(EncoderPins, 2434, 5); //(EncoderPin, EncoderFullTurn, Diamter)
+  
+  ConfigMotor(MotorPins, Pwm, 6); //(Motores, Pwm, Stby)
+  Encoder(EncoderPins, 1234, 5); //(EncoderPin, EncoderFullTurn, Diamter)
+  /*=======================
+       Manual de uso:
+  =======================*/
+  /*=======================
+          Motores
+  ========================*/
+  //  ConfigMotor Realiza la cofiguración inicial de los motores.
+  //  MotorPins: Establecer los pines fisicos del tb6612fng.
+  //  Pwm: Establece los pines que cuenten con PWM.
+  //  Stby: Pin que permite inicializar el Driver.
+  
+  /*=======================
+          Encoder
+  ========================*/
+  //  EncoderPins: Pines A y B del encoder fisicos.
+  //  EncoderFullturn: Este parametro lo dara el Datasheed es cuantos pulsos completa una vuelta.
+  //  Diamter: Permite realizar el calculo de la velocidad lineal.
+
 }
 void loop() {
-  LinealMotor(40, 255); // (Distance, BaseSpeed)
-  AngularMotor(90, 255); // (Angle, BaseSpeed)
+  /*==========================
+   LinealMotor & AngularMotor
+  ===========================*/
+  //  LinealMotor (TargetDistance, BaseSpeed)
+  //  AngularMotor (TargetAngle, BaseSpeed)
+  //  BaseSpeed permite avanzar o girar a la velocidad que deseemos.
+  LinealMotor(40, 255);
+  AngularMotor(90, 255);
 
 }
