9e99889313ed20c8103b309d60d1808fb3ac72cf
Diegopv07 <diego.porras@correounivalle.edu.co>
2025-05-02 21:10:22 -0500

    Agrego Comunicación Bluetooth a Arduino Final

--- Arduino/SARU-G2/ComunicacionArduino.ino
+++ Arduino/SARU-G2/ComunicacionArduino.ino
@@ -1,3 +1,20 @@
-void funcion_modularC() {
-    Serial.println("Hola desde Comunicación .ino");
+#include "BluetoothSerial.h"
+
+BluetoothSerial SerialBT;
+
+void setup() {
+  Serial.begin(115200);
+  delay(500); // Espera antes de imprimir
+  SerialBT.begin("ESP32_Chat"); 
+  Serial.println("Bluetooth iniciado. Esperando conexión...");
+}
+
+void loop() {
+  if (SerialBT.available()) {
+    String recibido = SerialBT.readStringUntil('\n');
+    Serial.print("Mensaje recibido desde MATLAB: ");
+    Serial.println(recibido);
+
+    SerialBT.println("Mensaje recibido: " + recibido);
+  }
 }
